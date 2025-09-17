5e5a429d89f2574b3cb16881002ff336b2e4eded
Marcin Makarewicz <marcin@makarewicz.eu>
2025-09-17 22:24:53 +0200

    fix: update deps

--- apps/main/tailwind.config.js
+++ apps/main/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     // Add the external packages that are using Tailwind CSS
     './node_modules/@vercel/examples-ui/**/*.js',
-    './node_modules/@acme/design-system/**/*.js',
-    './node_modules/@acme/pages/**/*.js',
+    './node_modules/@acme/design-system/dist/**/*.js',
+    './node_modules/@acme/pages/dist/**/*.js',
   ],
 }
